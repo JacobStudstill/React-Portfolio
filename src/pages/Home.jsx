d2c0316c083a743da35a8970f86e819cc0193cac
JacobStudstill <jacob.studstill@gmail.com>
2023-11-16 11:57:25 -0500

    Added the island 3d model and assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
           POPUP
@@ -12,7 +13,11 @@ const Home = () => {
       camera={{near: 0.1, far:1000}}
       >
         <Suspense fallback={<Loader />}>
-
+          <directionalLight />
+          <ambientLight />
+          <spotLight />
+          <pointLight />
+          <hemisphereLight />
         </Suspense>
 
       </Canvas>
